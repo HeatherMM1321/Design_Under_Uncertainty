1513b626d318247f1f5b85e0c80751899e37b68f
HeatherMM1321 <millerh@oregonstate.edu>
2020-05-06 15:44:49 -0700

    working on part 2/3

--- HW/HW3/HW3.py
+++ HW/HW3/HW3.py
@@ -4,6 +4,8 @@
 
 import numpy as np
 from scipy.stats import norm
+from numpy.polynomial.hermite import hermgauss
+from numpy.polynomial.legendre import leggauss
 
 ####################################################
 # Given Constants
@@ -34,17 +36,13 @@ Awa = 2.0074e-006  # cross sectional area of armature winding (m^2)
 Awf = 0.2749e-6  # cross sectional area of field coil winding (m^2)
 
 #########################################################################################
+# Part 1  : Using FORM
+
 
 def calculate_weight(diameter, length, rho_cu, rho_fe):
     # calculate the weight of the motor
     weight = (rho_cu * (Awa*Lwa + Awf*Lwf) + rho_fe * length * np.pi * pow(diameter + ds, 2)) - 22
     return weight
-
-# variable info
-variables = {"diameter": (0.075, 0.005),
-             "length": (0.095, 0.005),
-             "rho_cu": (8.94e3, 100),
-             "rho_fe": (7.98e3, 100)}
 
 
 def finite_diff(variables):
@@ -70,6 +68,7 @@ def finite_diff(variables):
 
     return [d_dD, d_dL, d_dcu, d_dfe]
 
+
 def update_eq(u, variables):
 
     variable_mus = [variables["diameter"][0], variables["length"][0], variables["rho_cu"][0],
@@ -92,19 +91,78 @@ def update_eq(u, variables):
 
     return u_k
 
+def u_update(u, diff_threshold, itterations):
 
-if __name__ == "__main__":
-    u = [0, 0, 0, 0]
     diff = 1
     k = 1
-    while diff > 0.001 and k < 20:
+    while diff > diff_threshold and k < itterations:
         uk = update_eq(u, variables)
         diff = abs(np.linalg.norm(uk) - np.linalg.norm(u))
         u = uk
-        prob = norm.cdf(np.linalg.norm(u))
+        beta = np.linalg.norm(u)
+        prob = norm.cdf(np.linalg.norm(beta))
 
         print("Iteration #", k, ":\n", u, "\nIntermediate Probability", prob, "\nIntermediate Beta",
               np.linalg.norm(u), "\n")
         k += 1
 
 
+
+#####################################################################################################
+# Part 2 : Using Full Tensor Numerical Integration Method
+# 3 nodes per uncertain input
+# same uncertainties as problem 1
+
+def convert_norm(number_of_nodes, variables):
+
+    variable_mus = [variables["diameter"][0], variables["length"][0], variables["rho_cu"][0],
+                               variables["rho_fe"][0]]
+    variable_sigmas = [variables["diameter"][1], variables["length"][1], variables["rho_cu"][1],
+                               variables["rho_fe"][1]]
+
+    points, weights = hermgauss(number_of_nodes)
+    variable_points =[]
+    variable_weights = [i / np.sqrt(np.pi) for i in weights]
+
+    for j in range(len(variables)):
+        variable_points.append([(np.sqrt(2) * i * variable_sigmas[j]) + variable_mus[j] for i in points])
+
+    return variable_points, variable_weights
+
+
+def convert_uniform(number_of_nodes, variables):
+
+    variable_lower = [variables["diameter"][0], variables["length"][0], variables["rho_cu"][0],
+                               variables["rho_fe"][0]]
+    variable_upper = [variables["diameter"][1], variables["length"][1], variables["rho_cu"][1],
+                               variables["rho_fe"][1]]
+
+    points, weights = leggauss(number_of_nodes)
+    variable_points =[]
+    variable_weights = [i / 2 for i in weights]
+
+    for j in range(len(variables)):
+        variable_points.append([(variable_upper[j] - variable_lower[j]) * i / 2 +
+                                (variable_upper[j] + variable_lower[j]) / 2 for i in points])
+
+    return variable_points, variable_weights
+if __name__ == "__main__":
+
+    # variable info for part 1 & 2
+    variables = {"diameter": (0.075, 0.005),
+                 "length": (0.095, 0.005),
+                 "rho_cu": (8.94e3, 100),
+                 "rho_fe": (7.98e3, 100)}
+    # u = [0, 0, 0, 0]
+    # problem_1 = u_update(u, .001, 20)
+
+
+    # Part 2
+    #print(calculate_full_factorial("norm", 3, variables))
+
+    # Part 3
+    # uniform distributions
+    # variables = {"diameter": (0.065, 0.085),
+    #              "length": (0.085, 0.105),
+    #              "rho_cu": (8840, 9040),
+    #              "rho_fe": (7880, 8080)}
