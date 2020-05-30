1caa2864affb9338549ac7d86fbcbf2b726da4ed
HeatherMM1321 <millerh@oregonstate.edu>
2020-05-30 14:42:32 -0700

    Fixed the issue with the usage data csv. Now has blank spaces instead of 0s when the stove/fuel wasnt present in householf

--- HW/Project/ME615_Project.py
+++ HW/Project/ME615_Project.py
@@ -26,75 +26,36 @@ filepaths = ['HH_38_2018-08-26_15-01-40_processed_v3.csv',
              'HH_371_2018-08-17_15-31-52_processed_v2.csv'
              ]
 
-
-def find_household_number():
-    '''This will find the household numbers associated with each file.'''
-
-    households = []
-
-    for file in filepaths:
-        number = file.split("_")[1]
-        households.append(number)
-    return households
-
-
-def info():
-    '''List of all stoves and fuels in study.'''
-
-    all_stoves = []
-    all_fuels = []
-
-    for file in filepaths:
-        df_stoves, stoves, fuels = convert.reformat_olivier_files(file)
-        data = household.Household(df_stoves, stoves, fuels)
-        for s in data.stoves:
-            if s == '3pierres' or s == '3':
-                s = '3stone'
-            if s not in all_stoves:
-                all_stoves.append(s)
-        for f in data.fuels:
-            if f not in all_fuels:
-                all_fuels.append(f)
-
-    return all_stoves, all_fuels
-
-
 if __name__ == "__main__":
 
-    households = find_household_number()
     study_stoves, study_fuels = info()
 
     df_usage = pd.DataFrame(columns=['Day', 'Household', 'telia usage (mins)', 'om30 usage (mins)',
                                      '3stone usage (mins)', 'malgchch usage (mins)',
                                      'firewood usage (kg)', 'charcoal usage (kg)', 'lpg usage (kg)'])
     # look through each of the files
-    for i, house in enumerate(households):
-        df_stoves, stoves, fuels = convert.reformat_olivier_files(filepaths[i])
-        data = household.Household(df_stoves, stoves, fuels)
+    for i, file in enumerate(filepaths):
+        df_stoves, stoves, fuels, hh_id = convert.reformat_olivier_files(file)
+        data = household.Household(df_stoves, stoves, fuels, hh_id)
 
         # getting household cooking and fuel use info
         stove_data = data.cooking_duration()
         fuel_data = data.fuel_usage()
 
-        # study is 3 days long so we will iterate through 3 times (could be more elegant)
-        for j in range(3):
-            info = {'Day': j+1, 'Household': house}
-            # go through each of the stoves in the study and then put record how much that stove was
-            # used on that day in that household
-            for stv in study_stoves:
-                if stv in stove_data.columns:
-                    time = stove_data[stv].values[j]
-                    info.update({stv + ' usage (mins)': time})
-                else:
-                    info.update({stv + ' usage (mins)': 0})
-            # same process as with the stoves
-            for fls in study_fuels:
-                if fls in fuel_data.columns:
-                    weight = fuel_data[fls].values[j]
-                    info.update({fls + ' usage (kg)': weight})
-                else:
-                    info.update({fls + ' usage (kg)': 0})
+        days = round(data.study_duration.total_seconds()/86400)
 
+        for j in range(days):
+            info = {'Day': j+1, 'Household': hh_id}
+            for s in stoves:
+                time = stove_data[s].values[j]
+                if s == '3pierres' or s == '3':
+                    s = '3stone'
+                info.update({s + ' usage (mins)': time})
+                # else:
+                #     info.update({stv + ' usage (mins)': 'NA'})
+            for f in fuels:
+                time = fuel_data[f].values[j]
+                info.update({f + ' usage (kg)': time})
             # append the dataframe with the complete information for that household on that day
             df_usage = df_usage.append(info, ignore_index=True)
 
